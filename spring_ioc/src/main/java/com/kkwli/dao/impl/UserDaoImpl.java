256cd9e794e7fc2e61446b56443ac3717b884bef
kkwli <kkwlikeyou@gmail.com>
2021-08-29 09:54:54 +0800

    初始化和销毁

--- spring_ioc/src/main/java/com/kkwli/dao/impl/UserDaoImpl.java
+++ spring_ioc/src/main/java/com/kkwli/dao/impl/UserDaoImpl.java
@@ -3,8 +3,20 @@ package com.kkwli.dao.impl;
 import com.kkwli.dao.UserDao;
 
 public class UserDaoImpl implements UserDao {
+    public UserDaoImpl() {
+        System.out.println("UserDaoImpl创建...");
+    }
+
     @Override
     public void save() {
         System.out.println("save running...");
     }
+    public void init() {
+        System.out.println("初始化方法...");
+    }
+    public void destroy() {
+        System.out.println("销毁方法...");
+    }
+
 }
+
